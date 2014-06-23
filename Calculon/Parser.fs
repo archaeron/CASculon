d533940517c9b23675b620bf7fe6403cda4e91d9
Joris Morger <jorismorger@gmail.com>
2014-06-23 18:16:26 +0200

    added matrix parser

--- Calculon/Parser.fs
+++ Calculon/Parser.fs
@@ -6,9 +6,21 @@ open FParsec.CharParsers
 open Calculon.Types
 
 module Parser =
-    let numberParser =
+    let numberParser : Parser<Const, Unit>=
         pfloat |>> Number
 
+
+    let listBetweenSecondary pElement separator f = (spaces >>. sepBy (pElement .>> spaces) (pstring separator >>. spaces) |>> f)
+
+    let matrixBetweenDelimiters sOpen sClose secondarySeparator separator  pElement f =
+        between (pstring sOpen) (pstring sClose)
+            (spaces >>. sepBy (listBetweenSecondary pElement secondarySeparator f .>> spaces) (pstring separator >>. spaces) |>> f)
+
+    let matrixParser =
+        matrixBetweenDelimiters "[" "]" "," ";" numberParser List
+
+    let g = run matrixParser "[1,2,4;5,6,7]"
+        
     let listBetweenStrings sOpen sClose separator pElement f =
         between (pstring sOpen) (pstring sClose)
                 (spaces >>. sepBy (pElement .>> spaces) (pstring separator >>. spaces) |>> f)
@@ -19,5 +31,4 @@ module Parser =
     let t = run listParser "[1, 2, 3]"
 
     let parse s =
-
-        Assignment ("x", (Multiplication (Constant (Integer 5), Constant (Integer 4))))
+        Assignment ("x", (Multiplication (Constant (Number 5), Constant (Number 4))))
