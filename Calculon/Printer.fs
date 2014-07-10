e856156db5af22fc20bd0680b7cb9b41f9de4379
archaeron <whatamaps@gmail.com>
2014-07-11 00:57:45 +0200

    the printer can now print most of the types

--- Calculon/Printer.fs
+++ Calculon/Printer.fs
@@ -3,20 +3,28 @@ module Calculon.Printer
 open System
 open Calculon.Types
 
-let printConstant =
+let rec printConstant =
     function
-    | Number i -> sprintf "%f" i
-    | Complex (real, complex) -> "Hallo"
-    | BigInt r -> "Hallo"
-    | Matrix m -> "Hallo"
+    | Number i ->
+        if Math.Floor i = i
+        then
+            sprintf "%i" <| int i
+        else
+            sprintf "%f" i
+    | Complex (real, complex) -> "Complex"
+    | BigInt r -> "BigInt"
+    | Matrix m ->
+        let listOfStrings = m |> List.map (List.map print)
+        sprintf "%A" listOfStrings
 
-
-let rec print =
+and print =
     function
-    | Exponentiation (a, b) -> sprintf "%s^%s" (print a) (print b)
-    | Multiplication (a, b) -> sprintf "%s * %s" (print a) (print b)
-    | Addition (a, b) -> sprintf "%s + %s" (print a) (print b)
-    | Variable s -> s
+    | Exponentiation (a, b) -> sprintf "(%s ^ %s)" (print a) (print b)
+    | Multiplication (a, b) -> sprintf "(%s * %s)" (print a) (print b)
+    | Division (a, b) -> sprintf "(%s / %s)" (print a) (print b)
+    | Addition (a, b) -> sprintf "(%s + %s)" (print a) (print b)
+    | Subtraction (a, b) -> sprintf "(%s - %s)" (print a) (print b)
+    | Identifier i -> i
     | Assignment (s, expr) -> sprintf "%s = %s" s (print expr)
     | Call (a, b) -> sprintf "%s (%s)" (print a) (print b) 
     | Constant c -> printConstant c
