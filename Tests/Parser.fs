fe60a69fda1a188f595cd8269960316a6fda515a
archaeron <whatamaps@gmail.com>
2014-08-31 15:28:29 +0200

    add a test for an addition

--- Tests/Parser.fs
+++ Tests/Parser.fs
@@ -8,7 +8,13 @@ open Calculon.Parser
 type ParserTests() = 
 
     [<Test>]
-    member x.TestCase() =
+    member x.SimpleNumber() =
         let expected: Choice<Expr, String> = 5.0 |> Number |> Constant |> Choice1Of2
         Assert.AreEqual(expected, parse "5")
 
+    [<Test>]
+    member x.Addition() =
+        let number1 = 5.0 |> Number |> Constant
+        let number2 = 9.0 |> Number |> Constant
+        let expr: Choice<Expr, String> = Addition (number1, number2) |> Choice1Of2
+        Assert.AreEqual(expr, parse "5 + 9")
