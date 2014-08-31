0922d6ceedf53e4b94868affa7e952ff563333ca
archaeron <whatamaps@gmail.com>
2014-08-31 08:52:37 +0200

    add a first test

---
+++ Tests/Parser.fs
@@ -0,0 +1,14 @@
+﻿namespace Tests
+open System
+open NUnit.Framework
+open Calculon.Types
+open Calculon.Parser
+
+[<TestFixture>]
+type ParserTests() = 
+
+    [<Test>]
+    member x.TestCase() =
+        let expected: Choice<Expr, String> = 5.0 |> Number |> Constant |> Choice1Of2
+        Assert.AreEqual(expected, parse "5")
+
