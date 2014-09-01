4d88d9887eb60061747883ca987a1128f205aa4f
archaeron <whatamaps@gmail.com>
2014-09-01 14:34:45 +0200

    add some more tests

--- Tests/Parser.fs
+++ Tests/Parser.fs
@@ -18,3 +18,21 @@ type ParserTests() =
         let number2 = 9.0 |> Number |> Constant
         let expr: Choice<Expr, String> = Addition (number1, number2) |> Choice1Of2
         Assert.AreEqual(expr, parse "5 + 9")
+
+    [<Test>]
+    member x.Division() =
+        let number1 = 5.0 |> Number |> Constant
+        let number2 = 9.0 |> Number |> Constant
+        let expr: Choice<Expr, String> = Division (number1, number2) |> Choice1Of2
+        Assert.AreEqual(expr, parse "5 / 9")
+
+    [<Test>]
+    member x.Precedence() =
+        let number1 = 4.0 |> Number |> Constant
+        let number2 = 2.0 |> Number |> Constant
+        let number3 = 3.0 |> Number |> Constant
+        let mult = Multiplication (number2, number3)
+        let addition = Addition (number1, mult)
+
+        let expr: Choice<Expr, String> = addition |> Choice1Of2
+        Assert.AreEqual(expr, parse "4 + 2 * 3")
