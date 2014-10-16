f8cf4a7a86c19ac10fb8969f208a9237274e4569
Nicolas Gagliani <whatamaps@gmail.com>
2014-10-16 20:02:43 +0200

    dont pause after executing the fake script

--- build.bat
+++ build.bat
@@ -4,4 +4,3 @@ if not exist packages\FAKE\tools\Fake.exe (
 	".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 )
 "packages\FAKE\tools\Fake.exe" build.fsx "%1"
-pause
